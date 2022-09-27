f8f3fdd1037195148848108c958f99903e9bc4c3
shrikant@flexli.in <shrikant@flexli.in>
2022-09-27 12:00:30 +0530

    Initial commit

---
+++ Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Basket.API.Entities;
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _repository;
+
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _repository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+    }
+
+    
+    [HttpGet("{userName}",Name = "GetBasket")]
+    [ProducesResponseType(typeof(ShoppingCart),(int) HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+    {
+        var basket = await _repository.GetBasket(userName);
+        return Ok(basket ?? new ShoppingCart(userName));
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(ShoppingCart),(int) HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody]ShoppingCart shoppingCart)
+    {
+        return Ok(await _repository.UpdateBasket(shoppingCart));
+    }
+    
+    [HttpDelete("{userName}", Name = "DeleteBasket")]
+    [ProducesResponseType(typeof(ShoppingCart),(int) HttpStatusCode.OK)]
+    public async Task<ActionResult<bool>> DeleteBasket(string shoppingCartId)
+    {
+        await _repository.DeleteBasket(shoppingCartId);
+        return Ok();
+    }
+}
---
+++ Services/Basket/Basket.API/Startup.cs
@@ -0,0 +1,58 @@
+﻿
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.OpenApi.Models;
+
+namespace Catalog.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetValue<string>
+                    ("CacheSettings:ConnectionString");
+                
+            });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+            });
+
+            //services.AddHealthChecks()
+              //  .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], "MongoDb Health", HealthStatus.Degraded);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();                
+                
+            });
+        }
+    }
+}
